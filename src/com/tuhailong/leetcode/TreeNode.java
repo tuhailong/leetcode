d2b338081dcd177c171d18220a9ad6cc36622c63
tuhailong <tuhailong@gmail.com>
2019-11-27 09:02:08 +0800

    upload minimum-distance-between-bst-nodes solution

--- src/com/tuhailong/leetcode/TreeNode.java
+++ src/com/tuhailong/leetcode/TreeNode.java
@@ -4,12 +4,12 @@ package com.tuhailong.leetcode;
  * @author tuhailong
  * @since 2019-10-15
  */
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
 
-    TreeNode(int x) {
+    public TreeNode(int x) {
         val = x;
     }
 }
---
+++ src/com/tuhailong/leetcode/tree/LeetCode783.java
@@ -0,0 +1,51 @@
+package com.tuhailong.leetcode.tree;
+
+import com.tuhailong.leetcode.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+/**
+ * 给定一个二叉搜索树的根结点 root, 返回树中任意两节点的差的最小值。
+ * 示例：
+ * 输入: root = [4,2,6,1,3,null,null]
+ * 输出: 1
+ * 解释:
+ * 注意，root是树结点对象(TreeNode object)，而不是数组。
+ * 给定的树 [4,2,6,1,3,null,null] 可表示为下图:
+ *           4
+ *         /   \
+ *       2      6
+ *      / \
+ *     1   3
+ * 最小的差值是 1, 它是节点1和节点2的差值, 也是节点3和节点2的差值。
+ * 注意：
+ * 二叉树的大小范围在 2 到 100。
+ * 二叉树总是有效的，每个节点的值都是整数，且不重复。
+ * 链接：https://leetcode-cn.com/problems/minimum-distance-between-bst-nodes
+ *
+ * @author tuhailong
+ * @since 2019-11-26
+ */
+public class LeetCode783 {
+    public int minDiffInBST(TreeNode root) {
+        int min = Integer.MAX_VALUE;
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode lastNode = null;
+        while (root != null || !stack.isEmpty()) {
+           while (root != null) {
+               stack.push(root);
+               root = root.left;
+           }
+           TreeNode node = stack.pop();
+           if (lastNode != null) {
+               int diff = Math.abs(node.val - lastNode.val);
+               min = Math.min(min, diff);
+           }
+           lastNode = node;
+           root = node.right;
+        }
+        return min;
+    }
+}
