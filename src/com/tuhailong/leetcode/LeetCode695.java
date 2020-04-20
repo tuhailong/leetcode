68e55a70f7488dd6a700ee304a582cfa105e16ae
tuhailong <tuhailong@gmail.com>
2020-04-20 23:31:20 +0800

    Update LeetCode695.java

--- src/com/tuhailong/leetcode/LeetCode695.java
+++ src/com/tuhailong/leetcode/LeetCode695.java
@@ -2,7 +2,7 @@ package com.tuhailong.leetcode;
 
 /**
  * 岛屿的最大面积
- * 
+ *
  * 给定一个包含了一些 0 和 1 的非空二维数组 grid 。
  * 一个岛屿是由一些相邻的 1 (代表土地) 构成的组合，这里的「相邻」要求两个 1 必须在水平或者竖直方向上相邻。
  * 你可以假设 grid 的四个边缘都被 0（代表水）包围着。
@@ -67,7 +67,7 @@ public class LeetCode695 {
         }
         /** 3.标记当前结点坐标为已访问区域 **/
         grid[rid][cid] = ISLAND_HAS_VISITED;
-        /** 递归调用DFS方法 **/
+        /** 4.递归调用DFS方法 **/
         return area(grid, rid - 1, cid)
                 + area(grid, rid + 1, cid)
                 + area(grid, rid, cid - 1)
